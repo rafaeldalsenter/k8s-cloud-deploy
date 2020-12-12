73b7cedd084e5f49eb5206767cc58590725c1ccb
Rafael Dalsenter <rafaeldalsenter@gmail.com>
2020-12-12 20:53:21 -0300

    Primeiros códigos do GCP

--- cloud_providers/gcp.py
+++ cloud_providers/gcp.py
@@ -1,6 +1,14 @@
+import os
 from cloud_providers.cloud_provider import CloudProvider
 
 class Gcp(CloudProvider):
+
+    def __init__(self):
+        self.__id_project = os.getenv('PROJECT_ID')
+        self.__region = os.getenv('REGION')
+
+        if(self.__id_project == None or self.__region == None):
+            raise Exception("Set the PROJECT_ID and REGION environment variables")
 
     def configure(self):
         print('entrou no gcp')
